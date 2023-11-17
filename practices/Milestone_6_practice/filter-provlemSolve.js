3592e6415a2e79352337e2aa77fe63818a97e228
mirza-azam <mirzaazam884@gmail.com>
2023-11-17 11:59:33 +0600

    for and for of loop

--- practices/Milestone_6_practice/filter-provlemSolve.js
+++ practices/Milestone_6_practice/filter-provlemSolve.js
@@ -3,3 +3,8 @@
 const arr = [33, 50, 79, 78, 90, 101, 30];
 const result = arr.filter(i => i % 10 === 0);
 console.log(result);
+
+//find problem solve.
+const arr2 = [33, 50, 79, 78, 90, 101, 30];
+const result2 = arr2.find(i => i % 10 === 0);
+console.log(result);
