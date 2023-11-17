9b535d9c82e2346076fb99d25ce900a3f373e90c
mirza-azam <mirzaazam884@gmail.com>
2023-11-17 15:11:54 +0600

    problem solve using reduce function

--- practices/Milestone_6_practice/reduce.js
+++ practices/Milestone_6_practice/reduce.js
@@ -1,27 +1,25 @@
 
+// const arrr = [1, 9, 17, 22];
 // // let sum = 0;
-// const reduce = arr.reduce(0, ar => {
-    //     return sum + ar;
-    // })
-    
-    
-    
-    // console.log(arr);
-    
-    
-    //for loop 
-    const arr = [1, 9, 17, 22];
-    let sum = 0;
-    for ( let i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
-    console.log('only for loop result: ',sum);
+// const reduce = arrr.reduce(sum, ar => {
+//     return sum + ar;
+// })
+// console.log(arrr);
 
 
-    //fot of loop
-    const arr2 = [1, 9, 17, 22];
-    let sum1 = 0;
-    for ( i of arr2){
-        sum1 += i;
-    }
-    console.log('for of loop result: ',sum1);
+//for loop 
+const arr = [1, 9, 17, 22];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log('only for loop result: ', sum);
+
+
+//fot of loop
+const arr2 = [1, 9, 17, 22];
+let sum1 = 0;
+for (i of arr2) {
+    sum1 += i;
+}
+console.log('for of loop result: ', sum1);
